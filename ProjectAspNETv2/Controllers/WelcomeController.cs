ed28912f4c88f4987bd9148113b12b1eaf2d568d
azziti <azziti2000@gmail.com>
2022-01-12 18:24:35 +0100

    added welcome template

---
+++ ProjectAspNETv2/Controllers/WelcomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAspNETv2.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
