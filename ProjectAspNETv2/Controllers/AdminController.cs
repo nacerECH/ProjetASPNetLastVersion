30dbacc76872d05d6556cdb5931d9593262c071f
nacerECH <nassreddineechafai@gmail.com>
2022-01-12 12:21:32 +0100

    Add project files.

---
+++ ProjectAspNETv2/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAspNETv2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            if (id == 1) { return View("Home"); }
+            else if (id == 2) { return View("Vendeurs"); }
+            else if (id == 3) { return View("Products"); }
+            else if (id == 4) { return View("Statistic"); }
+            else if (id == 5) { return View("Historique"); }
+
+            else
+            {
+                return View("Home");
+            }
+
+
+        }
+    }
+}
---
+++ ProjectAspNETv2/Controllers/ProprietaireController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAspNETv2.Controllers
+{
+    public class ProprietaireController : Controller
+    {
+        // GET: Proprietaire
+        public ActionResult Index(int id)
+        {
+            if (id == 1) { return View("Home"); }
+            else if (id == 2) { return View("Products"); }
+
+            else if (id == 3) { return View("Statistic"); }
+            else if (id == 4) { return View("Historique"); }
+            else if (id == 5) { return View("Support"); }
+
+            else
+            {
+                return View("Home");
+            }
+
+
+        }
+    }
+}
